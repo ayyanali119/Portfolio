04cca31cf608ab218adeae188682035896bf9f4f
Ayyan Ali <150801601+ayyanali119@users.noreply.github.com>
2024-04-14 11:02:31 +0500

    Update project.tsx

--- src/app/component/project.tsx
+++ src/app/component/project.tsx
@@ -54,7 +54,7 @@ const Project = () => {
             className="absolute inset-0 w-full h-full 
             object-cover 
             object-center"
-            src={require("../../../public/assests/project/Capture3.PNG")}
+            src={require("../../../public/assests/project/Capture3.png")}
           />
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
             <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
